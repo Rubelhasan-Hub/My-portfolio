374c9826f82aedcf036a048a289809dd2976036f
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-04-12 14:57:19 +0600

    make many sections where my sections are aded and cract a component that is navabar and degien the section

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Navbar from './layout/Navbar'
+import About from './section/About'
+import Contact from './section/Contact'
+import Experience from './section/Experience'
+import Hero from './section/Hero'
+import Projects from './section/Projects'
+import Testimonials from './section/Testimonials'
+
+function App() {
+
+  return (
+    <div className='min-h-screen overflow-x-hidden'>
+      <Navbar/>
+      <main>
+        <Hero/>
+        <About/>
+        <Projects/>
+        <Experience/>
+        <Testimonials/>
+        <Contact/>
+      </main>
+    </div>
+  )
+}
+
+export default App
