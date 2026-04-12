c38e6e044548c403cc239203d6e4e6651a14b222
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-04-12 20:02:08 +0600

    make the hero section completed added the text buttons annimatinos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './layout/Navbar'
 import About from './section/About'
 import Contact from './section/Contact'
 import Experience from './section/Experience'
-import Hero from './section/Hero'
+import { Hero } from './section/Hero'
 import Projects from './section/Projects'
 import Testimonials from './section/Testimonials'
 
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,17 +1,27 @@
-const Hero = () => {
+import { ArrowRight, Download, GitBranchIcon } from "lucide-react";
+import Button from "../components/Button";
+import { AnimatedBorderButton } from "../components/AnimatedBorderButton";
+import { FaGithub } from "react-icons/fa";
+import { GrLinkedin } from "react-icons/gr";
+import { SiDiscord } from "react-icons/si";
+
+export const Hero = () => {
     return (
         <section className="relative min-h-screen flex items-center overflow-hidden">
-            {/* Background image */}
+            {/* Bg */}
             <div className="absolute inset-0">
-                <img className="w-full h-full object-cover opacity-40" src="/hero-bg.jpg" alt="hero image of my website it is a small light effect" />
+                <img
+                    src="/hero-bg.jpg"
+                    alt="Hero image"
+                    className="w-full h-full object-cover opacity-40"
+                />
                 <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background" />
             </div>
+
             {/* Green Dots */}
-
             <div className="absolute inset-0 overflow-hidden pointer-events-none">
-                {[...Array(30)].map(() => (
-
-                    <div
+                {[...Array(30)].map((_, i) => (
+                    <div key={i}
                         className="absolute w-1.5 h-1.5 rounded-full opacity-60"
                         style={{
                             backgroundColor: "#20B2A6",
@@ -26,14 +36,14 @@ const Hero = () => {
             </div>
 
             {/* Content */}
-            <div className="container mx-auto px-6 pt-32 pb-20 relative z-10">
-                <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <div className="container mx-auto px-6 pt-32 pb-20 relative z-10 flex">
+                <div className="grid lg:grid-cols-2 gap-20">
                     {/* Left Column - Text Content */}
                     <div className="space-y-8">
                         <div className="animate-fade-in">
                             <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm text-primary">
                                 <span className="w-2 h-2 bg-primary rounded-full animate-pulse" />
-                                Full Stack Web Developer
+                                Software Engineer • React Specialist
                             </span>
                         </div>
 
@@ -54,11 +64,46 @@ const Hero = () => {
                                 applications that users love.
                             </p>
                         </div>
+
+                        {/* Call to action section */}
+                        <div className="flex gap-2">
+                            <Button size="lg">
+                                Contact Me <ArrowRight className="w-5 h-5"></ArrowRight>
+                            </Button>
+                            <button>
+                                <AnimatedBorderButton>
+                                    Download CV <Download></Download>
+                                </AnimatedBorderButton>
+                            </button>
+                        </div>
+
+                        {/* Social LInks */}
+                        <div className="flex items-center gap-4 animate-fade-in animation-delay-400">
+                            <span className="text-sm text-muted-foreground">Follow: </span>
+                            {[
+                                { icon: FaGithub, href: "#" },
+                                { icon: GrLinkedin, href: "#" },
+                                { icon: SiDiscord, href: "#" },
+                            ].map((social, idx) => (
+                                <a key={idx} href={social.href} className="p-2 rounded-full glass hover:bg-primary/10 hover:text-primary transition-all duration-300"><social.icon className="w-5 h-5"/></a>
+                            ))}
+                        </div>
+
+
+                    </div>
+
+                    {/* Right side for image */}
+                    <div className="relative animate-fade-in">
+                        <div>
+                            <div className="flex justify-center rounded-3xl p-2">
+                                <img src="/public/hero-person.jpg" alt="Rubel Hasan's image" className="w-[65%] object-right lg:aspect-7/10 glow-border glass rounded-3xl"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
         </section>
     );
 };
-
-export default Hero;
